3b88e301d3ccebee98db746ebd81a83e385f3ffb
Mike Belanger <mikejamesbelanger@gmail.com>
2023-12-02 12:43:49 -0500

    Attempt at adding a shared connection pool to main thread

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ pub mod view;
 
 use view::*;
 
-use sqlx::{postgres::PgPoolOptions, Error, PgPool};
+use sqlx::{postgres::PgPoolOptions, Error, PgPool, pool::PoolConnectionMetadata, Postgres};
 use std::env;
 
 async fn get_pool() -> Result<PgPool, Error> {
@@ -20,10 +20,11 @@ async fn get_pool() -> Result<PgPool, Error> {
 }
 
 #[web::get("/")]
-async fn index() -> impl web::Responder {
-    let pool = get_pool().await.unwrap();
+async fn index(
+    pool: web::types::State<PgPool>,
+) -> impl web::Responder {
     let todos = sqlx::query_as::<_, Todo>("SELECT title, content FROM todo")
-        .fetch_all(&pool)
+        .fetch_all(&*pool)
         .await
         .unwrap();
 
@@ -39,8 +40,10 @@ async fn index() -> impl web::Responder {
 
 #[ntex::main]
 async fn main() -> std::io::Result<()> {
-    web::HttpServer::new(|| {
+    let pool = get_pool().await.expect("error starting connection pool");
+    web::HttpServer::new(move || {
         web::App::new()
+            .state(pool.clone())
             .service(index)
             .service(fs::Files::new("/", "./static_assets/"))
     })
